24c81d8ec8ff31cfb3339cace20f22c718b24b91
Juan Vallejo-Nagera <juanvallejo-nagera@Juans-MacBook-Pro.local>
2017-11-20 18:05:16 +0100

    Login y Signup hecho

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const layouts      = require('express-ejs-layouts');
-const mongoose     = require('mongoose');
+const bodyParser = require('body-parser');
+const layouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const dbURL = 'mongodb://localhost/geekmeet';
 
 
-mongoose.connect('mongodb://localhost/geekmeet');
+
+mongoose.connect(dbURL);
 
 const app = express();
 
@@ -28,8 +32,21 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(layouts);
 
+app.use(session({
+  secret: 'geekmeetdev',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}));
+
+require('./passport/passport')(app);
+
 const index = require('./routes/index');
+const auth = require('./routes/auth');
+
 app.use('/', index);
+app.use('/', auth);
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router  = express.Router();
 
-/* GET home page. */
+/* Landing Page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/* Main User Page*/
+
+router.get('/userProfile', (req, res, next) => {
+  res.render('user/index');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
